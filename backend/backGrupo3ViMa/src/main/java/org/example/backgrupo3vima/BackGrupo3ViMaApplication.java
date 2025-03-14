56bf390e5e1758d00580124e5063b14d77aa1f31
EnzoAA004 <117104663+EnzoAA004@users.noreply.github.com>
2025-03-14 17:44:55 -0300

    Primer commit

---
+++ backend/backGrupo3ViMa/src/main/java/org/example/backgrupo3vima/BackGrupo3ViMaApplication.java
@@ -0,0 +1,13 @@
+package org.example.backgrupo3vima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackGrupo3ViMaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackGrupo3ViMaApplication.class, args);
+    }
+
+}
---
+++ backend/backGrupo3ViMa/src/test/java/org/example/backgrupo3vima/BackGrupo3ViMaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.backgrupo3vima;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackGrupo3ViMaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
