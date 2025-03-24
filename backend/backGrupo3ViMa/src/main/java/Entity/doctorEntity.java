fd9b128db45ac05b4aa00887b99de3732c5a0cd1
FranEFabrello <frfabrello@uade.edu.ar>
2025-03-24 17:52:46 -0300

    Entidad Doctor (fran)

--- backend/backGrupo3ViMa/src/main/java/Entity/doctorEntity.java
+++ backend/backGrupo3ViMa/src/main/java/Entity/doctorEntity.java
@@ -6,10 +6,26 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
 
+import java.sql.Timestamp;
+
 @Data
 @Entity
 public class doctorEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    private String nombre;
+
+    private String apellido;
+
+    private String especialidad;
+
+    private String telefono;
+
+    private String email;
+
+    private Timestamp horarioInicio;
+
+    private Timestamp horarioFin;
 }
