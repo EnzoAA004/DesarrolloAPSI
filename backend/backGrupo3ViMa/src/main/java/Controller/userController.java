daf6fb8011fd9b2618d519cf53c6558bb55d1283
EnzoAA004 <117104663+EnzoAA004@users.noreply.github.com>
2025-04-02 16:44:52 -0300

    avance de clases de usuario
    capas de entidad, servicio e implementacion del servicio

--- backend/backGrupo3ViMa/src/main/java/Controller/userController.java
+++ backend/backGrupo3ViMa/src/main/java/Controller/userController.java
@@ -1,10 +1,13 @@
 package Controller;
 
+import Entity.userEntity;
 import Service.userService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/Usuario")
@@ -13,6 +16,48 @@ public class userController {
 
     @Autowired
     private userService userService;
+
+    @GetMapping
+    public ResponseEntity<List<userEntity>> todosLosUsuarios(){
+        List<userEntity> userList = userService.getAllUsers();
+        return ResponseEntity.ok(userList);
+    }
+
+    @PutMapping("/{id}/nombre")
+    public ResponseEntity<userEntity> actualizarNombre(@PathVariable Long id, @RequestBody String nombre) {
+        userEntity usuario = userService.actualizarNombre(id, nombre);
+        return ResponseEntity.ok(usuario);
+    }
+
+    @PutMapping("/{id}/apellido")
+    public ResponseEntity<userEntity> actualizarApellido(@PathVariable Long id, @RequestBody String apellido) {
+        userEntity usuario = userService.actualizarApellido(id, apellido);
+        return ResponseEntity.ok(usuario);
+    }
+
+    @PutMapping("/{id}/edad")
+    public ResponseEntity<userEntity> actualizarEdad(@PathVariable Long id, @RequestBody int edad) {
+        userEntity usuario = userService.actualizarEdad(id, edad);
+        return ResponseEntity.ok(usuario);
+    }
+
+    @PutMapping("/{id}/fechaNacimiento")
+    public ResponseEntity<userEntity> actualizarFechaNacimiento(@PathVariable Long id, @RequestBody String fechaNacimiento) {
+        userEntity usuario = userService.actualizarFechaNacimiento(id, fechaNacimiento);
+        return ResponseEntity.ok(usuario);
+    }
+
+    @PutMapping("/{id}/genero")
+    public ResponseEntity<userEntity> actualizarGenero(@PathVariable Long id, @RequestBody String genero) {
+        userEntity usuario = userService.actualizarGenero(id, genero);
+        return ResponseEntity.ok(usuario);
+    }
+
+    @PutMapping("/{id}/obraSocial")
+    public ResponseEntity<userEntity> actualizarObraSocial(@PathVariable Long id, @RequestBody String obraSocial) {
+        userEntity usuario = userService.actualizarObraSocial(id, obraSocial);
+        return ResponseEntity.ok(usuario);
+    }
 
 
 }
--- backend/backGrupo3ViMa/src/main/java/Service/userServiceImpl.java
+++ backend/backGrupo3ViMa/src/main/java/Service/userServiceImpl.java
@@ -1,9 +1,15 @@
 package Service;
 
 
+import Entity.obrasSocialesEntity;
+import Entity.userEntity;
+import Repository.obrasSocialesRepository;
 import Repository.userRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Service
 public class userServiceImpl implements userService {
@@ -11,4 +17,55 @@ public class userServiceImpl implements userService {
     private userRepository userRepository;
     @Autowired
     private userService userService;
+
+    @Autowired
+    private obrasSocialesRepository obrasSocialesRepository;
+
+    @Override
+    public List<userEntity> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public userEntity actualizarNombre(Long id, String nombre) {
+        userEntity usuario = userRepository.findById(id).orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
+        usuario.setNombre(nombre);
+        return userRepository.save(usuario);
+    }
+
+    @Override
+    public userEntity actualizarApellido(Long id, String apellido) {
+        userEntity usuario = userRepository.findById(id).orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
+        usuario.setApellido(apellido);
+        return userRepository.save(usuario);
+    }
+
+    @Override
+    public userEntity actualizarEdad(Long id, int edad) {
+        userEntity usuario = userRepository.findById(id).orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
+        usuario.setEdad(edad);
+        return userRepository.save(usuario);
+    }
+
+    @Override
+    public userEntity actualizarFechaNacimiento(Long id, String fechaNacimiento) {
+        userEntity usuario = userRepository.findById(id).orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
+        usuario.setFechaNacimiento(LocalDate.parse(fechaNacimiento));
+        return userRepository.save(usuario);
+    }
+
+    @Override
+    public userEntity actualizarGenero(Long id, String genero) {
+        userEntity usuario = userRepository.findById(id).orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
+        usuario.setGenero(genero);
+        return userRepository.save(usuario);
+    }
+
+    @Override
+    public userEntity actualizarObraSocial(Long id, String obraSocialId) {
+        userEntity usuario = userRepository.findById(id).orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
+        obrasSocialesEntity obraSocial = (obrasSocialesEntity) obrasSocialesRepository.findById((int) Long.parseLong(obraSocialId)).orElseThrow(() -> new RuntimeException("Obra social no encontrada"));
+        usuario.setObraSocial(obraSocial);
+        return userRepository.save(usuario);
+    }
 }
